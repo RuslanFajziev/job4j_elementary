db00b3b1639166029542b514ef946000b2f8a4f2
FaizievRT <rus23f@mail.ru>
2020-10-28 22:50:08 +0300

    3.3. Разбор первой программы. [#398789]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ruslan Faiziev");
+        System.out.println("20.01.1985");
+    }
+}
