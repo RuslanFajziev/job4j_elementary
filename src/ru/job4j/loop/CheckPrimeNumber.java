a76efa85660903a8087566b510bc0086c99a98e0
FaizievRT <rus23f@mail.ru>
2020-11-05 20:29:28 +0300

    5.5. Простое число [#398829]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 1 || number == 2 || number == 3) {
+            return prime;
+        } else if (number == 4) {
+            return false;
+        }
+        for (int n = 3; n < number; n++) {
+            if (number % n == 0 && number % 2 == 0 && number % number == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
---
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,68 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class CheckPrimeNumberTest {
+    @Test
+    public void when5() {
+        boolean rsl = CheckPrimeNumber.check(5);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when4() {
+        boolean rsl = CheckPrimeNumber.check(4);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when1() {
+        boolean rsl = CheckPrimeNumber.check(1);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when8() {
+        boolean rsl = CheckPrimeNumber.check(8);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when7() {
+        boolean rsl = CheckPrimeNumber.check(7);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when19() {
+        boolean rsl = CheckPrimeNumber.check(19);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when173() {
+        boolean rsl = CheckPrimeNumber.check(173);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when174() {
+        boolean rsl = CheckPrimeNumber.check(174);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when1483() {
+        boolean rsl = CheckPrimeNumber.check(1483);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when1484() {
+        boolean rsl = CheckPrimeNumber.check(1484);
+        assertThat(rsl, is(false));
+    }
+}
