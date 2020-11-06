b5ddf6084395252d11aeac5afa4225f8bae444f9
FaizievRT <rus23f@mail.ru>
2020-11-06 21:17:21 +0300

    5.7. Ипотека [#398828]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,13 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        for (int n = 1; n > 0; n++) {
-            year = n;
+        while (amount > 0) {
             double amountPercent = amount + (amount * percent / 100);   // кредит+проценты
             amount = amountPercent - salary;                  // остаток
-            if (amount < 0) {
-                break;
-            }
+            year++;
         }
         return year;
     }
