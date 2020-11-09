ec4cc503e8802c130f70d11a3524075069164a8a
FaizievRT <rus23f@mail.ru>
2020-11-09 21:28:59 +0300

    6.0.1. Объявление массива. [#398765]

--- src/ru/job4j/loop/DigitSum.java
+++ src/ru/job4j/loop/DigitSum.java
@@ -4,7 +4,7 @@ public class DigitSum {
     public static int sum(int num) {
         int sum = 0;
         String txt = String.valueOf(num);
-        int cycle = txt.length() -1;
+        int cycle = txt.length() - 1;
         while (cycle >= 0) {
             sum += Character.getNumericValue(txt.charAt(cycle));
             cycle--;
